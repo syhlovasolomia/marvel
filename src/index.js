12bfc5e750a041f3147343fe42755d5e4a229438
syhlovasolomia <syhlova.solomia@gmail.com>
2024-10-30 21:29:14 +0200

    сreated a file that connects with api and receives data from the server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const marvelService = new MarvelService();
 
+marvelService
+  .getAllCharacters()
+  .then(res => res.data.results.forEach(item => console.log(item.name)));
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
