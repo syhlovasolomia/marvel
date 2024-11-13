c45c470f498b50b9d488680afba3b8beef357c37
syhlovasolomia <syhlova.solomia@gmail.com>
2024-11-13 20:28:00 +0200

    added didUpdate component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import AppHeader from "../AppHeader/AppHeader";
+import RandomChar from "../RandomChar/RandomChar";
+import CharList from "../CharList/CharList";
+import CharInfo from "../CharInfo/CharInfo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import decoration from '../../resources/img/vision.png'
+
+class App extends Component {
+
+  state = {
+    selectedChar: null
+  }
+
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AppHeader />
+        <main>
+          <RandomChar />
+          <div className="char__content">
+            <CharList onCharSelected={this.onCharSelected} />
+            <CharInfo charId={this.state.selectedChar} />
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    )
+  }
 }
-
 export default App;
