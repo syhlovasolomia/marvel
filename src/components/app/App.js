92d0bf3141ab923df109693dc3e04237dc15e8c2
syhlovasolomia <syhlova.solomia@gmail.com>
2024-12-16 22:07:26 +0200

    "App.js" & "CharList.js" the functionality was rewritten into react hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,19 +7,14 @@ import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
-class App extends Component {
+const App = () => {
 
-    state = {
-        selectedChar: null
+    const [selectedChar, setChar] = useState(null);
+
+    const onCharSelected = (id) => {
+        setChar(id);
     }
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
-
-    render() {
         return (
             <div className="app">
                 <AppHeader />
@@ -29,10 +24,10 @@ class App extends Component {
                     </ErrorBoundary>
                     <div className="char__content">
                         <ErrorBoundary>
-                            <CharList onCharSelected={this.onCharSelected} />
+                            <CharList onCharSelected={onCharSelected} />
                         </ErrorBoundary>
                         <ErrorBoundary>
-                            <CharInfo charId={this.state.selectedChar} />
+                            <CharInfo charId={selectedChar} />
                         </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision" />
@@ -40,6 +35,5 @@ class App extends Component {
             </div>
         )
     }
-}
 
 export default App;
