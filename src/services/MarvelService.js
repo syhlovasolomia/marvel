9b05fc8799b421e89ff03c0230e52ec9d8bb4972
syhlovasolomia <syhlova.solomia@gmail.com>
2024-12-23 21:17:23 +0200

    added "Comics" page

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -11,12 +11,22 @@ const useMarvelService = () => {
     const getAllCharacters = async (offset = _basedOffSet) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter);
-    }
+    };
 
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
-    }
+    };
+
+    const getAllComics = async (offset = 0) => {
+        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
+    };
+
+    const getComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    };
 
     const _transformCharacter = (char) => {
         return {
@@ -26,11 +36,24 @@ const useMarvelService = () => {
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
-            comics: char.comics.items
-        }
-    }
+            comics: char.comics.items,
+        };
+    };
 
-    return { loading, error, clearError, getAllCharacters, getCharacter }
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || 'There is no description',
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            language: comics.textObjects[0]?.language || 'en-us',
+            price: comics.prices[0].price
+                ? `${comics.prices[0].price}$` : 'Not available',
+        };
+    };
+
+    return { loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComics }
 }
 
 export default useMarvelService;
